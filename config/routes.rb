94d1a4d7141b338ec007445c20aabe1fdb474b34
maruyamaryo <t.t.uncle0803@gmail.com>
2021-11-10 20:53:15 +0900

    tops_new_fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   root 'sessions#new'
-  get '/tops', to: 'tops#index'
+  #get '/tops', to: 'tops#index'
   resources :customers
+  resources :tops
 
 end
