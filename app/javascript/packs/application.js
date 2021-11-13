896cde18ad9135a4163629f561ed58bdb26d3696
maruyamaryo <t.t.uncle0803@gmail.com>
2021-11-13 14:53:35 +0900

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import '../stylesheets/application';
 //= require jquery
 //= require moment
 //= require fullcalendar
-
+//= require bootstrap.min #追加
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
