847b8d1ebd5c461401957c194ac41d3046f19ed9
maruyamaryo <t.t.uncle0803@gmail.com>
2021-11-13 14:42:46 +0900

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,11 @@ require("channels")
 import 'bootstrap';
 import '../stylesheets/application';
 
+//= require jquery
+//= require moment
+//= require fullcalendar
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
